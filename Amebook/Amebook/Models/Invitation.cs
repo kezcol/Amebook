cfb01e7e79212399f58672b002bfc3d5acee4ee7
kamtag <kamil.tagowski@gmail.com>
2015-11-16 13:38:03 +0100

    Cryptography Random

---
+++ Amebook/Amebook/Models/Invitation.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Web;
+
+namespace Amebook.Models
+{
+    public class Invitation
+    {
+        public String HashCode { get; set; }
+        public DateTime CreationDate { get; set; }
+        public int DurationTime = 15 * 60;
+        public String AccountID { get; set; }
+        public String InvitationKey { get; set; }
+
+        public Invitation()
+        {
+            InvitationKey = Amebook.Crypto.CryptoRandom.GenerateKey(30);
+        }
+    }
+}
