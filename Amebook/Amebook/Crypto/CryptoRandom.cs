221cc46bbfeb0b7ca92366ad4994bf9c2c5fd877
kamtag <kamil.tagowski@gmail.com>
2015-11-19 10:33:20 +0100

    Dodano losowe sortowanie tablicy

--- Amebook/Amebook/Crypto/CryptoRandom.cs
+++ Amebook/Amebook/Crypto/CryptoRandom.cs
@@ -22,20 +22,31 @@ namespace Amebook.Crypto
             return IntegerArray;
 
         }
+        private static int next()
+        {
+            byte[] tab = new byte[4];
+            provider.GetBytes(tab);
+            return BitConverter.ToInt32(tab, 0);
+
+        }
         public static String GenerateKey(int keyLenght)
         {
             string KEY_CHARS_LCASE = "abcdefgijkmnopqrstwxyz";
             string KEY_CHARS_UCASE = "ABCDEFGHJKLMNPQRSTWXYZ";
             string KEY_CHARS_NUMERIC = "0123456789";
             string KEY_CHARS_SPECIAL = "*$-+?_&=!%@";
-            string PASSSTABLE = KEY_CHARS_LCASE + KEY_CHARS_NUMERIC + KEY_CHARS_SPECIAL + KEY_CHARS_UCASE;
+            char[] PASSSTABLE = (KEY_CHARS_LCASE + KEY_CHARS_NUMERIC + KEY_CHARS_SPECIAL + KEY_CHARS_UCASE).ToCharArray();
             int[] valTab = generateIntTab(keyLenght, PASSSTABLE.Length);
-            StringBuilder txt = new StringBuilder();
-            char[] passArray = PASSSTABLE.ToArray();
-            foreach (var x in valTab)
+            var tmp = valTab.ToList();
+            var shuffled = tmp.OrderBy(a => next());
+            var shuffledArray = shuffled.ToArray();
+            StringBuilder key = new StringBuilder();
+            foreach (var x in shuffledArray)
             {
-                txt.Append(passArray[x]);
+                key.Append(PASSSTABLE[x]);
             }
-            return txt.ToString();
+            return key.ToString();
         }
+
     }
+}
