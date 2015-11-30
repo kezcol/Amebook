9582c89a1ab07a76c9129e6775078df29b9c29a1
kamtag <kamil.tagowski@gmail.com>
2015-11-30 16:35:24 +0100

    AES Algorithm Implementation

--- Amebook/Amebook/Crypto/TextEncryption.cs
+++ Amebook/Amebook/Crypto/TextEncryption.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
+using System.Security.Cryptography;
 using System.Web;
 
 namespace Amebook.Crypto
@@ -8,15 +10,79 @@ namespace Amebook.Crypto
     public class TextEncryption
     {
         // TODO
-        public String encrypt(String RSAPublicKey, String text)
+        static byte[] AesEncrypt(String text, byte[] Key, byte[] IV)
         {
-            //TODO
-            return "";
+            byte[] cipher;
+            if (text == null || text.Length <= 0)
+                throw new ArgumentNullException("plainText");
+            if (Key == null || Key.Length <= 0)
+                throw new ArgumentNullException("Key");
+            if (IV == null || IV.Length <= 0)
+                throw new ArgumentNullException("Key");
+            using (Aes aesAlg = Aes.Create())
+            {
+                aesAlg.IV = IV;
+                aesAlg.Key = Key;
+                ICryptoTransform encryptor = aesAlg.CreateEncryptor(aesAlg.Key, aesAlg.IV);
+                using (MemoryStream msEncrypt = new MemoryStream())
+                {
+                    using (CryptoStream csEncrypt = new CryptoStream(msEncrypt, encryptor, CryptoStreamMode.Write))
+                    {
+                        using (StreamWriter swEncrypt = new StreamWriter(csEncrypt))
+                        {
+                            swEncrypt.Write(text);
+                        }
+                        cipher = msEncrypt.ToArray();
+                    }
+                }
+            }
+            return cipher;
         }
-        public String decrypt(String RSAPrivateKey, String text)
+        static string AesDecrypt(byte[] encryptedMsg, byte[] Key, byte[] IV)
         {
-            //TODO
-            return "";
+            string text = null;
+            if (encryptedMsg == null || encryptedMsg.Length <= 0)
+                throw new ArgumentNullException("cipherText");
+            if (Key == null || Key.Length <= 0)
+                throw new ArgumentNullException("Key");
+            if (IV == null || IV.Length <= 0)
+                throw new ArgumentNullException("Key");
+            using (Aes aesAlg = Aes.Create())
+            {
+                aesAlg.IV = IV;
+                aesAlg.Key = Key;
+                ICryptoTransform decryptor = aesAlg.CreateDecryptor(aesAlg.Key, aesAlg.IV);
+                using (MemoryStream msDecrypt = new MemoryStream(encryptedMsg))
+                {
+                    using (CryptoStream csDecrypt = new CryptoStream(msDecrypt, decryptor, CryptoStreamMode.Read))
+                    {
+                        using (StreamReader srDecrypt = new StreamReader(csDecrypt))
+                        {
+                            text = srDecrypt.ReadToEnd();
+                        }
+
+                    }
+                }
+            }
+            return text;
+        }
+        void AesTest()
+        {
+            try
+            {
+                string text = "Here is some data to encrypt, lol";
+                using (Aes myAes = Aes.Create())
+                {
+                    byte[] encrypted_data = AesEncrypt(text, myAes.Key, myAes.IV);
+                    string plaintext = AesDecrypt(encrypted_data, myAes.Key, myAes.IV);
+                    //Console.WriteLine(plaintext);
+                }
+              //  Console.ReadKey();
+            }
+            catch (Exception e)
+            {
+               // Console.WriteLine(e.Message);
+            }
         }
     }
 }
